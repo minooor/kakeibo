86ce1963081079499d16f465c8a94717045d1b75
EC2 Default User <ec2-user@ip-172-31-13-209.ec2.internal>
2022-03-14 11:15:01 +0000

    [Add] 削除機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -34,6 +34,12 @@ class BooksController < ApplicationController
     end
   end
 
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
   private
    def book_params
      params.require(:book).permit(:year, :month, :inout, :category, :amount)
