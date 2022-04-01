0b783f2e680b7012ce3aa6542e87846002a8dba4
minooor <minorilax13@gmail.com>
2022-04-01 03:18:09 +0000

    Add Calendar

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,41 @@
+class BlogsController < ApplicationController
+  def index
+    @blogs = Blog.all
+    @blog = Blog.new
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def show
+    @blog = Blog.find(params[:id])
+  end
+
+  def create
+    Blog.create(blog_params)
+    redirect_to blogs_path
+  end
+
+  def destroy
+    @blog = Blog.find(params[:id])
+    @blog.destroy
+    redirect_to blogs_path, notice: '削除しました'
+  end
+
+  def edit
+    @blog = Blog.find(params[:id])
+  end
+
+  def update
+    @blog = Blog.find(params[:id])
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: '編集しました'
+    end
+  end
+
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :content, :start_time)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_13_120717) do
+ActiveRecord::Schema.define(version: 2022_04_01_025057) do
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "start_time"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.integer "inout"
